73cbc94b9bd76ba62ec86c1622e16d80248db1d7
nialke <cyzia5@gmail.com>
2023-10-13 14:05:13 +0200

    feature(user): add send mail after create user [#3]

--- src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
+++ src/Core/User/Application/Command/CreateUser/CreateUserHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Core\User\Application\Command\CreateUser;
 
+use App\Common\Mailer\MailerInterface;
 use App\Core\User\Domain\Repository\UserRepositoryInterface;
 use App\Core\User\Domain\User;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -10,12 +11,20 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 class CreateUserHandler
 {
     public function __construct(
+        private readonly MailerInterface $mailer,
         private readonly UserRepositoryInterface $userRepository
     ) {}
 
     public function __invoke(CreateUserCommand $command): void
     {
-        $this->userRepository->save(new User($command->email));
+        $email = $command->email;
+        $this->userRepository->save(new User($email));
         $this->userRepository->flush();
+
+        $this->mailer->send(
+            $email,
+            'Konto zostało utworzone',
+            'Zarejestrowano konto w systemie. Aktywacja konta trwa do 24h'
+        );
     }
 }
